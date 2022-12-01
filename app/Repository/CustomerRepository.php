0d96e872482edd56a1203cb2ba9f9a209b1873ab
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2022-12-01 15:48:46 -0400

    refactor: mudei a forma de busca dos clientes

--- app/Repository/CustomerRepository.php
+++ app/Repository/CustomerRepository.php
@@ -16,7 +16,7 @@ class CustomerRepository
     {
         $parameters = '%'.$request->name.'%';
        
-        $customer = $this->customer->where('name','like', $parameters)->first();
+        $customer = $this->customer->where('name','like', $parameters)->first(); //to do essa busca está errada, tenho que trazer todos os nomes que encontrar, mas estou trazendo só o primeiro usando o first
         return view('app.showCustomer', ['customer' => $customer]);
     }
 }
